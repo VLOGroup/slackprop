2f7c53e15ae0f86ae3a6cdce63dfacb2052eac14
Christian Reinbacher <reinbacher@icg.tugraz.at>
2017-03-17 09:55:31 +0100

    added licence

--- src/slack_prop.kernel.h
+++ src/slack_prop.kernel.h
@@ -1,3 +1,20 @@
+// This file is part of slack-prop.
+//
+// Copyright (C) 2017 Sasha Shekhovtsov <shekhovtsov at icg dot tugraz dot at>
+// Institute for Computer Graphics and Vision, Graz University of Technology
+// https://www.tugraz.at/institute/icg/teams/team-pock/
+//
+// dvs-panotracking is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or any later version.
+//
+// slack-prop is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 enum class return_types {reminder=0, total_withheld=1, total_free=2, split=3, recombine=4, resolve=5, messages=6};
